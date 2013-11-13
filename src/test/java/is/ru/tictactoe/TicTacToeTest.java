572a209a41a7ca9deabf2764a060102923a496ae
root <root@i-8510cb7a.greenqloud.internal>
2013-11-13 21:29:12 +0000

    WORKING COPY

--- src/test/java/is/ru/tictactoe/TicTacToeTest.java
+++ src/test/java/is/ru/tictactoe/TicTacToeTest.java
@@ -2,5 +2,11 @@ package is.ru.tictactoe;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 public class TicTacToeTest{
-       
+  
+	@Test
+	public void greetResultInHello(){
+			
+		
+	}     
 }
+
