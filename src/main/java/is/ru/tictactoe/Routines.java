020294bb6f28866861c6eaa4c40ea6ef9e88190f
aglikall <egillornsig@gmail.com>
2013-11-16 02:10:22 +0000

    sdf

---
+++ src/main/java/is/ru/tictactoe/Routines.java
@@ -0,0 +1,10 @@
+package is.ru.tictactoe;
+
+public class Routines{
+	public String greet()
+	{
+		//TicTacToe test = new TicTacToe();
+		return "Hello World!";
+		//System.out.println("EG KEYRI EDLILEGA");
+	}	
+}	
