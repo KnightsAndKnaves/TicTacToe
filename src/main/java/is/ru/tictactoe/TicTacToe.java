cae4c5a487f63114e845c95864d45a4322db6dfb
aglikall <egillornsig@gmail.com>
2013-11-23 03:21:27 +0000

    TicTacToe er master class

--- src/main/java/is/ru/tictactoe/TicTacToe.java
+++ src/main/java/is/ru/tictactoe/TicTacToe.java
@@ -4,5 +4,19 @@ public class TicTacToe {
 
 	public static void main(String[] args){
         System.out.println("Hello");
+        
+        Player p1 = new Player(1, 'X');
+		Player p2 = new Player(2, 'O');
+		Board board = new Board();
+		Game game = new Game(p1, p2, board);
+		for(int i = 0; i < 2; i++) {
+			turn(p1, board);
+			turn(p2, board);
+		}
+		turn(p1, board);
+		if(!game.end()) turn(p2, board);
+		if(!game.end()) turn(p1, board);
+		if(!game.end()) turn(p2, board);
+		if(!game.end()) turn(p1, board);
     }
 }
