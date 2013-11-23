dc5de75b1b7afc7442ea4707bfba69d9a1aaf0a2
aglikall <egillornsig@gmail.com>
2013-11-23 03:32:28 +0000

    asdf

--- src/main/java/is/ru/tictactoe/Board.java
+++ src/main/java/is/ru/tictactoe/Board.java
@@ -8,14 +8,19 @@ public class Board {
 		for(int i = 0; i < 9; i++)
 			board[i] = ' ';
 	}
-	
+	/*
 	public char[] getBoard() {
 		return board;
 	}
-	
+	*/
+	public void printBoard(){
+		System.out.println("");
+	}
+
 	public void addLetter(int pos, char letter) {
 		if(pos < 9 && pos >= 0 && canAdd(pos))
 			board[pos] = letter;
+		//TODO Hvað á að gerast ef ekki er hægt að merkja þennan reit 
 	}
 	
 	private boolean canAdd(int pos) {
