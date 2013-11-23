8cbe658fe34bd192fc7415b30d6daa6d285458d2
aglikall <egillornsig@gmail.com>
2013-11-23 03:22:47 +0000

    úff

--- src/main/java/is/ru/tictactoe/TicTacToe.java
+++ src/main/java/is/ru/tictactoe/TicTacToe.java
@@ -4,11 +4,12 @@ public class TicTacToe {
 
 	public static void main(String[] args){
         System.out.println("Hello");
-        
+
         Player p1 = new Player(1, 'X');
 		Player p2 = new Player(2, 'O');
 		Board board = new Board();
 		Game game = new Game(p1, p2, board);
+		/*
 		for(int i = 0; i < 2; i++) {
 			turn(p1, board);
 			turn(p2, board);
@@ -18,5 +19,6 @@ public class TicTacToe {
 		if(!game.end()) turn(p1, board);
 		if(!game.end()) turn(p2, board);
 		if(!game.end()) turn(p1, board);
+    	*/
     }
 }
